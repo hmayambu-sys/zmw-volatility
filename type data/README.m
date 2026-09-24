5463036755cf6d036fed10ef8eaa1f72bbb62c17
hmayambu-sys <hmayambu@gmail.com>
2026-09-24 12:00:45 +0200

    Create README.m

---
+++ type data/README.m
@@ -0,0 +1,7 @@
+# Data
+Source: Yahoo Finance ticker ZMW=X
+Period: 2015-01-02 to 2024-12-31
+Observations: 2604 daily closes
+File: zmw_usd_daily_2015_2024.csv
+Columns: Date, zmw_usd, log_return
+Processing: log_return = ln(Pt / Pt-1)
